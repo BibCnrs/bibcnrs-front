7b10e1d29c0fcf49272f1d84978d606c9653a16c
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-03-24 08:46:23 +0100

    refactor: remove App.tsx and use index.tsx instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import './index.scss';
 import { StrictMode } from 'react';
 import ReactDOMClient from 'react-dom/client';
-import App from './app/App';
 import { I18nextProvider } from 'react-i18next';
 import I18N from './app/locales/I18N';
+import Header from './app/components/header/Header';
+import Footer from './app/components/footer/Footer';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container);
@@ -10,7 +12,8 @@ const root = ReactDOMClient.createRoot(container);
 root.render(
     <StrictMode>
         <I18nextProvider i18n={I18N}>
-            <App />
+            <Header />
+            <Footer />
         </I18nextProvider>
     </StrictMode>,
 );
