0eafdac11d8819f8f18ed169a7849e4d70c3b815
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-03-31 10:33:55 +0200

    build: update build system to output es2018 minified js

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), eslint()],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            ecma: 2018,
+            compress: {
+                ecma: 2018,
+            },
+            format: {
+                ecma: 2018,
+            },
+        },
+    },
 });
