7e03e0dcaea16e2431ab204b73df5ac7169721d2
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-05 10:08:01 +0200

    build: use manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ import eslint from 'vite-plugin-eslint';
 export default defineConfig({
     plugins: [react(), eslint()],
     build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('react')) return 'vendor-rct';
+                        if (id.includes('mui')) return 'vendor-mui';
+                        return 'vendor-all';
+                    }
+                }
+            }
+        },
         minify: 'terser',
         terserOptions: {
             ecma: 2018,
