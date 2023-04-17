2fb8746bea59fc4c1644fc5a32494dd19be4147c
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-17 09:11:48 +0200

    feat: update cms services

--- src/app/services/cms/CMS.ts
+++ src/app/services/cms/CMS.ts
@@ -3,12 +3,16 @@ import { createQuery, environment } from '../Environment';
 
 type Pages = 'home' | 'legal' | 'faq' | 'about';
 
-const doQuery = async <Page extends Pages>(page: Page): Promise<CMSResultDataType> => {
-    const response: Response = await fetch(
-        createQuery(environment.get.cms, {
-            page,
-        }),
-    );
+const doQuery = async <Page extends Pages>(page: Page, first = true): Promise<CMSResultDataType> => {
+    const query = first
+        ? createQuery(environment.get.cms, {
+              page,
+              first,
+          })
+        : createQuery(environment.get.cms, {
+              page,
+          });
+    const response: Response = await fetch(query);
     return await response.json();
 };
 
@@ -16,6 +20,6 @@ export const home = (): Promise<CMSResultDataType> => doQuery('home');
 
 export const legal = (): Promise<CMSResultDataType> => doQuery('legal');
 
-export const faq = (): Promise<CMSResultDataType> => doQuery('faq');
+export const faq = (): Promise<CMSResultDataType> => doQuery('faq', false);
 
 export const about = (): Promise<CMSResultDataType> => doQuery('about');
