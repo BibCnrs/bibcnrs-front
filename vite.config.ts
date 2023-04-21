fb41deff7bcc170e2220a79f5a02f95834ae9de3
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-21 09:39:51 +0200

    build: update dependencies and build system

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
+import md5 from 'md5';
 
 const regex = /(.*node_modules\/)([^\/]+)(.*)/;
+
 export default defineConfig({
-    plugins: [react(), eslint()],
+    plugins: [react(), eslint(), stylelint()],
     build: {
         rollupOptions: {
             output: {
                 manualChunks: (id, meta) => {
                     if (id.includes('node_modules')) {
-                        const name = id.match(regex)[2].replace('@', '');
-                        if (meta.getModuleInfo(id).isIncluded) return name;
+                        if (meta.getModuleInfo(id).isIncluded) {
+                            const hash = md5(
+                                id.match(regex)[2]
+                                    .replace('@', '')
+                            );
+                            return `vendor/${hash.slice(0, 8)}`;
+                        }
                     }
                 },
             },
