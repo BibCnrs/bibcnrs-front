c49dc46cf30c21c04fb718f4f599e7ec2fb2d03a
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-24 11:03:45 +0200

    feat (ux): Return to home if authentication is close without login

--- src/app/components/protectedroute/ProtectedRoute.tsx
+++ src/app/components/protectedroute/ProtectedRoute.tsx
@@ -1,14 +1,24 @@
 import { BibContext } from '../provider/ContextProvider';
 import Authentication from '../authentication/Authentication';
 import { ProtectedRouteProps } from '../../shared/types/props.types';
+import { RouteRoot } from '../../shared/Routes';
 import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     const { login } = useContext(BibContext);
     const [open, setOpen] = useState(false);
-    const handleClose = () => setOpen(false);
+    const [close, setClose] = useState(false);
+    const navigate = useNavigate();
+    const handleClose = () => {
+        setOpen(false);
+        setClose(true);
+    };
 
     useEffect(() => {
+        if (close && !login) {
+            navigate(RouteRoot);
+        }
         setOpen(!login);
     }, [login, open]);
 
