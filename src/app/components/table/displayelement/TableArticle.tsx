e1ff7e1c0b15f93e6ef30be08bdb4744eda5c9b3
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-05-16 11:44:57 +0200

    fix: fix bug with cache who make pagination not display article

--- src/app/components/table/displayelement/TableArticle.tsx
+++ src/app/components/table/displayelement/TableArticle.tsx
@@ -9,116 +9,64 @@ import { useContext, useEffect, useMemo, useState } from 'react';
 import type { ArticleResultDataType } from '../../../shared/types/data.types';
 import type { TableDisplayElementProps } from '../../../shared/types/props.types';
 
-const TableArticle = ({ data: dataIn }: TableDisplayElementProps<ArticleResultDataType>) => {
+const NoAccessArticle = ({ data }: { data: ArticleResultDataType }) => {
     const t = useTranslator();
-    const { search } = useContext(BibContext);
-    const [retrieve, setRetrieve] = useState(false);
-    const [missing, setMissing] = useState(false);
-    const [first, setFirst] = useState(true);
-
-    const {
-        data: dataRetrieve,
-        isFetching,
-        isLoading,
-    } = useQuery<ArticleResultDataType, any, ArticleResultDataType, any>({
-        queryKey: ['article_retrieve', missing, search.article.domain, dataIn.dbId, dataIn.an],
-        queryFn: async () => {
-            if (missing) {
-                if (search.article.domain) {
-                    const value = await retrieveFn(search.article.domain, dataIn.dbId, dataIn.an);
-                    setRetrieve(true);
-                    return value;
-                }
+    const urls = data.articleLinks.urls.filter((entry) => entry.name === 'Publisher URL');
+    return (
+        <OpenablePaper
+            Title={
+                <div className="table-list-title">
+                    {data.title} <i>({t('components.table.content.noAccess')})</i>
+                </div>
             }
-            return null;
-        },
-        keepPreviousData: true,
-        staleTime: 3600000, // 1 hour of cache
-        cacheTime: 3600000, // 1000 * 60 * 60
-    });
-
-    const data = useMemo(() => {
-        if (retrieve && !missing) {
-            return {
-                ...dataIn,
-                ...dataRetrieve,
-            } as ArticleResultDataType;
-        }
-        return dataIn;
-    }, [dataIn, dataRetrieve, missing, retrieve]);
-
-    useEffect(() => {
-        if (first) {
-            if (!data.authors || !data.doi || !data.source) {
-                setMissing(true);
+            SmallBody={null}
+            FullBody={
+                <dl className="table-list-body">
+                    {urls.length > 0 ? (
+                        <span>
+                            <dt>{t('components.table.content.publisherUrl')}</dt>
+                            <dd>
+                                {urls.map((urlEntry) => (
+                                    <a
+                                        className="link"
+                                        key={urlEntry.name}
+                                        href={urlEntry.url}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        {urlEntry.url}
+                                    </a>
+                                ))}
+                            </dd>
+                        </span>
+                    ) : null}
+                    {data.languages ? (
+                        <span>
+                            <dt>{t('components.table.content.languages')}</dt>
+                            <dd>
+                                {data.languages.map((lang) => (
+                                    <div key={lang}>{lang}</div>
+                                ))}
+                            </dd>
+                        </span>
+                    ) : null}
+                    <span>
+                        <dt>{t('components.table.content.accessNumber')}</dt>
+                        <dd>{data.an}</dd>
+                    </span>
+                    <span>
+                        <dt>{t('components.table.content.dbId')}</dt>
+                        <dd>{data.dbId}</dd>
+                    </span>
+                </dl>
             }
-        }
-        setFirst(false);
-    }, [data, first]);
+            small
+        />
+    );
+};
 
-    if ((missing && !retrieve) || first || isFetching || isLoading) {
-        return <SkeletonEntry animation="pulse" />;
-    }
-
-    if (!data.authors || !data.doi || !data.source) {
-        return (
-            <OpenablePaper
-                Title={
-                    <div className="table-list-title">
-                        {data.title} <i>({t('components.table.content.noAccess')})</i>
-                    </div>
-                }
-                SmallBody={null}
-                FullBody={
-                    <dl className="table-list-body">
-                        {data.articleLinks.urls.length > 0 &&
-                        data.articleLinks.urls.filter((entry) => entry.name === 'Publisher URL').length > 0 ? (
-                            <span>
-                                <dt>{t('components.table.content.publisherUrl')}</dt>
-                                <dd>
-                                    {data.articleLinks.urls.map((urlEntry) => {
-                                        if (urlEntry.name === 'Publisher URL') {
-                                            return (
-                                                <a
-                                                    className="link"
-                                                    key={urlEntry.name}
-                                                    href={urlEntry.url}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                >
-                                                    {urlEntry.url}
-                                                </a>
-                                            );
-                                        }
-                                        return null;
-                                    })}
-                                </dd>
-                            </span>
-                        ) : null}
-                        {data.languages ? (
-                            <span>
-                                <dt>{t('components.table.content.languages')}</dt>
-                                <dd>
-                                    {data.languages.map((lang) => (
-                                        <div key={lang}>{lang}</div>
-                                    ))}
-                                </dd>
-                            </span>
-                        ) : null}
-                        <span>
-                            <dt>{t('components.table.content.accessNumber')}</dt>
-                            <dd>{data.an}</dd>
-                        </span>
-                        <span>
-                            <dt>{t('components.table.content.dbId')}</dt>
-                            <dd>{data.dbId}</dd>
-                        </span>
-                    </dl>
-                }
-            />
-        );
-    }
-
+const Article = ({ data }: { data: ArticleResultDataType }) => {
+    const t = useTranslator();
     return (
         <OpenablePaper
             Title={
@@ -139,6 +87,66 @@ const TableArticle = ({ data: dataIn }: TableDisplayElementProps<ArticleResultDa
             FullBody={<div>{JSON.stringify(data)}</div>}
         />
     );
+};
+
+const TableArticle = ({ data: dataIn }: TableDisplayElementProps<ArticleResultDataType>) => {
+    const { search } = useContext(BibContext);
+    const [retrieve, setRetrieve] = useState(false);
+    const [missing, setMissing] = useState(false);
+    const [first, setFirst] = useState(true);
+
+    const {
+        data: dataRetrieve,
+        isFetching,
+        isLoading,
+        isSuccess,
+    } = useQuery<ArticleResultDataType, any, ArticleResultDataType, any>({
+        queryKey: ['article_retrieve', missing, search.article.domain, dataIn.dbId, dataIn.an],
+        queryFn: async () => {
+            if (missing) {
+                if (search.article.domain) {
+                    return await retrieveFn(search.article.domain, dataIn.dbId, dataIn.an);
+                }
+            }
+            return null;
+        },
+        keepPreviousData: true,
+        staleTime: 3600000, // 1 hour of cache
+        cacheTime: 3600000, // 1000 * 60 * 60
+    });
+
+    const data = useMemo(() => {
+        if (retrieve && !missing) {
+            return {
+                ...dataIn,
+                ...dataRetrieve,
+            } as ArticleResultDataType;
+        }
+        return dataIn;
+    }, [dataIn, dataRetrieve, missing, retrieve]);
+
+    useEffect(() => {
+        if (!data.authors || !data.doi || !data.source) {
+            setMissing(true);
+        }
+        setFirst(false);
+    }, [data]);
+
+    useEffect(() => {
+        if (isSuccess) {
+            setRetrieve(true);
+        }
+    }, [isSuccess]);
+
+    if ((missing && !retrieve) || first || isFetching || isLoading) {
+        return <SkeletonEntry animation="pulse" />;
+    }
+
+    if (!data.authors || !data.doi || !data.source) {
+        return <NoAccessArticle data={data} />;
+    }
+
+    return <Article data={data} />;
 };
 
 export default TableArticle;
