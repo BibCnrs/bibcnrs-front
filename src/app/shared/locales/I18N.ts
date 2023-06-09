3405e0e913fdde4189b2c0b07b6d8541d4578978
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-06-09 09:55:16 +0200

    feat: update label

--- src/app/shared/locales/I18N.ts
+++ src/app/shared/locales/I18N.ts
@@ -262,8 +262,8 @@ export const useLanguageKey = (): string => {
 };
 
 export const supportedLanguages: SupportedLanguages = [
-    { key: 'en', label: 'English' },
-    { key: 'fr', label: 'Français' },
+    { key: 'en', label: 'EN - English' },
+    { key: 'fr', label: 'FR - Français' },
 ];
 
 /**
