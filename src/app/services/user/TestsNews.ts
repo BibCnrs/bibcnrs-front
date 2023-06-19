c12ae8e1161512ba52df9ff41dfddbe94746d3c8
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-06-19 09:11:41 +0200

    fix: remove previously added services filtering and add filtering in getData function

--- src/app/services/user/TestsNews.ts
+++ src/app/services/user/TestsNews.ts
@@ -1,11 +1,10 @@
-import { getDomains, getFavoriteDomain } from './Session';
+import { getDomains } from './Session';
 import { createQuery, environment, json, throwIfNotOk } from '../Environment';
 import type { TestsNewsDataType } from '../../shared/types/data.types';
-import type { Institute } from '../../shared/types/types';
 
 type Pages = 'news' | 'tests';
 
-const doQuery = async <Page extends Pages>(page: Page, domain?: Institute): Promise<TestsNewsDataType> => {
+const doQuery = async <Page extends Pages>(page: Page): Promise<TestsNewsDataType> => {
     const query = createQuery(environment.get.account.testsNews, {
         domains: getDomains().join(','),
         page,
@@ -17,19 +16,9 @@ const doQuery = async <Page extends Pages>(page: Page, domain?: Institute): Prom
 
     throwIfNotOk(response);
 
-    const results = await json<TestsNewsDataType>(response);
-
-    let fallback = getFavoriteDomain();
-    if (!fallback) {
-        if (getDomains().length > 0) {
-            fallback = getDomains()[0];
-        }
-        fallback = '';
-    }
-
-    return results.filter((result) => result.domains.includes(domain ?? (fallback as string)));
+    return json<TestsNewsDataType>(response);
 };
 
-export const tests = (domain?: Institute): Promise<TestsNewsDataType> => doQuery('tests', domain);
+export const tests = (): Promise<TestsNewsDataType> => doQuery('tests');
 
-export const news = (domain?: Institute): Promise<TestsNewsDataType> => doQuery('news', domain);
+export const news = (): Promise<TestsNewsDataType> => doQuery('news');
